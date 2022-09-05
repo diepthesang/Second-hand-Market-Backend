bbef34a42365a137fe17def2857e115f96cd0fd6
diepthesang <diepthesang@gmail.com>
2022-09-05 19:33:37 +0700

    test data

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var morgan = require('morgan')
 var cors = require('cors');
 const authRoute = require('./src/routes/auth.route');
 const { handleErrRoute, handleErr } = require('./src/middlewares/handleErr.middleware');
+const db = require('./src/db/models');
 app.use(cors())
 app.options('*', cors())
 app.use(morgan('tiny'))
@@ -12,11 +13,20 @@ app.use(express.json())
 app.use(express.urlencoded({
     extended: true
 }));
-app.use('/', (req, res, next) => {
-    return res.status(200).json({
-        status: 200,
-        message: 'Server is running'
-    })
+app.use('/', async (req, res, next) => {
+    let user = await db.User.findOne(
+        {
+            where: {
+                email: 'leotime1010@gmail.com'
+            }
+        }
+    )
+
+    return res.json(
+        {
+            data: user
+        }
+    )
 })
 app.use('/auth', authRoute)
 
